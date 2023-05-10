31c7a67f7e29cabd32a50b33968010970f9fb68c
MRWAFF22 <rza28869@gmail.com>
2023-05-10 19:12:55 +0700

    Update title website

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@ import { RouterLink, RouterView } from 'vue-router'
   <div>
       <div class="text-center py-10 rounded-md" role="group">
         <nav>
-          <RouterLink class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-lime-600 rounded-lg mr-2 hover:bg-gray-100 hover:text-lime-700 focus:z-10 focus:ring-2 focus:ring-lime-700 focus:text-lime-700" to="/app">App</RouterLink>
           <RouterLink class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-lime-600 rounded-lg mr-2 hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-lime-700 focus:text-lime-700" to="/">Home </RouterLink> 
+          <RouterLink class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-lime-600 rounded-lg mr-2 hover:bg-gray-100 hover:text-lime-700 focus:z-10 focus:ring-2 focus:ring-lime-700 focus:text-lime-700" to="/app">App</RouterLink>
         </nav>
       </div>
     <RouterView />
