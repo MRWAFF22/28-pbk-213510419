408d9429ad8ce60cd4f5701aef5a9325f304ff65
MRWAFF22 <rza28869@gmail.com>
2023-05-10 19:18:03 +0700

    Menjadi lebih responsif pada mode desktop dan mobile

--- src/views/Appku.vue
+++ src/views/Appku.vue
@@ -31,7 +31,7 @@ function removeTodo(todo) {
   <div class="container mx-auto">
     <div class="text-center">
       <form @submit.prevent="addTodo">
-        <div class="mb-6 px-10 md:px-0">
+        <div class="mb-6 px-10 md:px-5">
           <label class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Tambahkan Kegiatan</label>
           <input type="text"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-lime-500 focus:border-lime-500 p-2.5 w-full lg:w-1/2"
@@ -41,18 +41,20 @@ function removeTodo(todo) {
           class="text-white bg-lime-700 hover:bg-lime-800 focus:outline-none focus:ring-4 focus:ring-lime-300 font-medium rounded-md text-sm px-5 py-2.5 text-center mr-2 mb-2">Tambahkan</button>
       </form>
     </div>
-    <ul
-      class="w-1/2 mt-4 flex-row mx-auto items-center text-sm font-medium text-gray-900 bg-lime-200 border border-slate-300 rounded-lg">
-      <li class="p-2" v-for="todo in filteredTodos" :key="todo.id">
-        <div class="items-center flex justify-center">
-          <input type="checkbox"
-            class="w-4 h-4 text-lime-600 bg-gray-100 border-gray-300 rounded focus:ring-lime-500 mr-2"
-            v-model="todo.done">
-          <span :class="{ done: todo.done }">{{ todo.text }}</span>
-          <button class="ml-2 rounded-lg bg-red-400 hover:bg-red-600" @click="removeTodo(todo)">✖</button>
-        </div>
-      </li>
-    </ul>
+    <div class="px-10 md:px-5">
+      <ul
+        class="w-full lg:w-1/2 mt-4 flex-row mx-auto items-center text-sm font-medium text-gray-900 bg-lime-200 border border-slate-300 rounded-lg">
+        <li class="p-2" v-for="todo in filteredTodos" :key="todo.id">
+          <div class="items-center flex justify-center">
+            <input type="checkbox"
+              class="w-4 h-4 text-lime-600 bg-gray-100 border-gray-300 rounded focus:ring-lime-500 mr-2"
+              v-model="todo.done">
+            <span :class="{ done: todo.done }">{{ todo.text }}</span>
+            <button class="ml-2 rounded-lg bg-red-400 hover:bg-red-600" @click="removeTodo(todo)">✖</button>
+          </div>
+        </li>
+      </ul>
+    </div>
     <div class="text-center">
       <button
         class="mt-4 text-white bg-lime-700 hover:bg-lime-800 focus:outline-none focus:ring-4 focus:ring-lime-300 font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2"
